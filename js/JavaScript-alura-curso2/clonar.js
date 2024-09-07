d63e130e66e98f1cb106c729b2c9610cc9aa37c6
João Victor <joaovictormdasilva676@gmail.com>
2024-09-07 08:43:58 -0300

    trabalhando com objetos

---
+++ js/JavaScript-alura-curso2/clonar.js
@@ -0,0 +1,5 @@
+const notas = [7, 7, 8, 9];
+const novaListaNota = [...notas, 10]
+
+console.log(notas);
+console.log(novaListaNota);
---
+++ js/javaScript-alura-curso3/objeto.js
@@ -0,0 +1,9 @@
+const listaCPFs = ["1234567891011", "9876543211011"];
+const infoPessoa = ["nome", "josé Silva", "idade", 32];
+console.log(infoPessoa[1]);
+const objPessoa ={
+    nome: "José Silva",
+    idade: 32,
+    cpf: "1234567891011",
+    turma: "JavaScript"
+}
